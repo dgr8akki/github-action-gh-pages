1b9a890e9a28e324895991435159195c9fbed8b6
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-09-28 13:30:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 if [ ! -n "$PUBLIC_PATH" ]; then
   PUBLIC_PATH=public
 fi
+if [ ! -n "$PUSH_BRANCH" ]; then
+  PUSH_BRANCH=gh-pages
+fi
 if [ ! -n "$GITHUB_TOKEN" ]; then
   echo "You need to supply GITHUB_TOKEN"
   exit 1
 fi
-gh-pages -d $PUBLIC_PATH -b gh-pages -u "github-actions-bot <support+actions@github.com>"
+gh-pages -d $PUBLIC_PATH -b $PUSH_BRANCH -u "github-actions-bot <support+actions@github.com>"
 retval=$?
 if [ $retval -ne 0 ]; then
   echo "gh-pages failed: $retval"
