9cb6cd3abb0e4f2e80cb2fe2053f5b2360107b96
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-10-01 11:51:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -n "$GITHUB_TOKEN" ]; then
   echo "You need to supply GITHUB_TOKEN"
   exit 1
 fi
-gh-pages -d $PUBLIC_PATH -b $PUSH_BRANCH -u "github-actions-bot <support+actions@github.com>"
+gh-pages -d $PUBLIC_PATH -b $PUSH_BRANCH -u "dgr8akki <pahujaaakash5@gmail.com>"
 retval=$?
 if [ $retval -ne 0 ]; then
   echo "gh-pages failed: $retval"
